755fa41cfa4b4d6331831266480617eec1a3d216
madhanv22 <madhankumarv40@gmail.com>
2024-10-16 15:56:34 +0530

    styles are added

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -57,8 +57,6 @@ const Content = () => {
 
   return (
     <main className='bg-gradient-body flex-container'>
-      <p className="text-lg font-medium mt-4 mx-4 text-gray-700">A simple and intuitive to-do app to help you manage your tasks efficiently.
-      </p>
       <div className='input-container'>
         <input 
           className='input-search'
@@ -68,7 +66,7 @@ const Content = () => {
           value={newItem}
           onChange={(e) => setNewItem(e.target.value)}
         />
-        <button className='add-button' onClick={handleAdd}>Add</button>
+        <button className='add-button' onClick={handleAdd}>ADD</button>
       </div>
       <ul className='list-container'>
         {items.length > 0 ? (
@@ -83,22 +81,22 @@ const Content = () => {
               <span className='items-list' style={item.checked ? { textDecoration: 'line-through' } : {}}>
                 {item.items}
               </span>
-              <button className='delete-button' onClick={() => handleDelete(item.id)}>Delete</button>
+              <button className='delete-button' onClick={() => handleDelete(item.id)}>x</button>
             </li>
           ))
         ) : (
           <p className='empty'>Your List Is Empty</p>
         )}
       </ul>
-        <div className="mt-6 p-4 bg-gray-100 rounded-lg shadow-md">
-          <h2 className="text-xl font-semibold text-gray-800 mb-2">How to Use the To-Do App</h2>
-          <ol className="list-decimal list-inside text-gray-700">
-              <li className="mb-2">Enter your task in the input field.</li>
-              <li className="mb-2">Click the <span className="font-bold">Add</span> button to add the task to your list.</li>
-              <li className="mb-2">Your task will appear in the list below.</li>
-              <li className="mb-2">Check the box next to a task to mark it as complete.</li>
-              <li>To delete a task, click the <span className="font-bold">Delete</span> button next to it.</li>
-          </ol>
+      <div className="how-to-use mt-6 p-4 rounded-lg bg-white bg-opacity-80 shadow-xl">
+        <h2 className="text-xl font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-600 to-pink-600 mb-2">How to Use the To-Do App</h2>
+        <ol className="list-decimal list-inside text-gray-700">
+          <li className="mb-2">Enter your task in the input field.</li>
+          <li className="mb-2">Click the <span className="font-bold">Add</span> button to add the task to your list.</li>
+          <li className="mb-2">Your task will appear in the list below.</li>
+          <li className="mb-2">Check the box next to a task to mark it as complete.</li>
+          <li>To delete a task, click the <span className="font-bold">Delete</span> button next to it.</li>
+        </ol>
       </div>
     </main>
   );
