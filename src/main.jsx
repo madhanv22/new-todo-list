9849b657cfbbdf7c10a805b8d4853d7ecb6bf252
madhanv22 <madhankumarv40@gmail.com>
2024-10-17 17:54:37 +0530

    break down the css components

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import './header.css'
+import './guide.css'
+import './main.css'
+import './input.css'
+import './items.css'
+import './buttons.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
