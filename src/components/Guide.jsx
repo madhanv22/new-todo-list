f815b9fd327e090f1939544b5578de91f32e5c26
madhanv22 <madhankumarv40@gmail.com>
2024-10-18 12:56:15 +0530

    body bg image and header img added

--- src/components/Guide.jsx
+++ src/components/Guide.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Guide = () => {
   return (
-    <div className="how-to-use mt-6 p-4 rounded-lg bg-white bg-opacity-80 shadow-xl">
-      <h2 className="text-xl font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-600 to-pink-600 mb-2">How to Use the To-Do App</h2>
-        <ol className="list-decimal list-inside text-gray-700">
+    <div className="how-to-use">
+      <h2>How to Use the To-Do App</h2>
+        <ol>
           <li className="mb-2">Enter your task in the input field.</li>
           <li className="mb-2">Click the <span className="font-bold">Add</span> button to add the task to your list.</li>
           <li className="mb-2">Your task will appear in the list below.</li>
