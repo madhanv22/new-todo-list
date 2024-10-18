787d2aad619f5a5f3f5bbb240afd8f9cf47abe32
madhanv22 <madhankumarv40@gmail.com>
2024-10-18 15:56:28 +0530

    almost finished

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css';
 import './header.css';
-import './css/guide.css';
+import './guide.css';
 import './css/main.css';
 import './css/input.css';
 import './css/items.css';
